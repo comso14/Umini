0cde745a8a6783802aa619dd415bd35b5c56e216
hyounglin Jun <bearics@gmail.com>
2018-04-13 07:58:25 +0900

    Add Test window
    The TestWindow is for testing new feature. It can make more windows in
    Test area.

--- Umini/Test/TestWindow.xaml.cs
+++ Umini/Test/TestWindow.xaml.cs
@@ -10,27 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-using Umini.Test;
-
-namespace Umini
+namespace Umini.Test
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for TestWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TestWindow : Window
     {
-        public MainWindow()
+        public TestWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Window window = new TestWindow();
-            window.Show();
         }
     }
 }

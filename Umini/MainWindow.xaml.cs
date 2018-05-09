fa41c2f3118d90995cee0acfb47e182542769d9d
hhhh245 <hhhh245@naver.com>
2018-05-09 16:33:31 +0900

    Test

--- Umini/MainWindow.xaml.cs
+++ Umini/MainWindow.xaml.cs
@@ -12,12 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Umini.Test.junpil;
 using Umini.ViewModel;
 using Umini.Test;
 using Player;
 using Umini.Test.mgh3326;
 using System.Web;
+using Umini.Test.hhhh24;
+
 
 namespace Umini
 {
@@ -44,10 +46,15 @@ namespace Umini
             Test_PlaylistParsing parsing = new Test_PlaylistParsing();
             Youtube youtube = parsing.ParsingYoutube(txtUrl.Text);
             MessageBox.Show(youtube.mTitle);
+            playlist.AppendText(youtube.mTitle+"\n");
 
         }
 
-   
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Test_play play = new Test_play();
+            play.YoutubePlay(txtUrl.Text);
+        }
     }
 
 }

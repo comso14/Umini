5c9a17e8b867b4aa5aa9f522ba591d5631d6f860
hhhh245 <hhhh245@naver.com>
2018-05-21 19:10:42 +0900

    Update buttons

--- Umini/MainWindow.xaml.cs
+++ Umini/MainWindow.xaml.cs
@@ -37,7 +37,6 @@ namespace Umini
     {
         public NowPlayingList mNowPlayingList;
         public Test_play play;
-
         public MainWindow()
         {
             InitializeComponent();
@@ -59,22 +58,63 @@ namespace Umini
             window.Show();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void btn_Play_Click(object sender, RoutedEventArgs e)
         {
-            BitmapImage bi = new BitmapImage(new Uri(mNowPlayingList.mMediaList[0].mImagePath, UriKind.RelativeOrAbsolute));
-            album.Source = bi;
-            MediaFile media = mNowPlayingList.mMediaList[0];
-            play.Music_Open(media.mPath);
-            txtNoPlayingInformation.AppendText("노래 제목 : " + media.mTitle + "\n가     수 : " + media.mArtist);
-            txtLyric.AppendText(media.mLyric);
+            if (mNowPlayingList.mMediaList[mNowPlayingList.mNowPlayingOrder].mPath != null)
+            {
+                BitmapImage bi = new BitmapImage(new Uri(mNowPlayingList.mMediaList[mNowPlayingList.mNowPlayingOrder].mImagePath, UriKind.RelativeOrAbsolute));
+                album.Source = bi;
+                MediaFile media = mNowPlayingList.mMediaList[mNowPlayingList.mNowPlayingOrder];
+                play.Music_Open(mNowPlayingList.mMediaList[mNowPlayingList.mNowPlayingOrder].mPath);
+                txtNoPlayingInformation.AppendText("노래 제목 : " + media.mTitle + "\n가     수 : " + media.mArtist);
+                txtLyric.AppendText(media.mLyric);
+                play.video.Position = new TimeSpan(0, 0, 0, 0,Convert.ToInt32(mNowPlayingList.mNowMediaPosition));
+                mNowPlayingList.mIsPlay = true;
+            }
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void btn_Pause_Click(object sender, RoutedEventArgs e)
         {
+            Pause();
+        }
+
+        public void Pause()
+        {
+            mNowPlayingList.mNowMediaPosition = play.CurPosition();
+            mNowPlayingList.mIsPlay = false;
             play.VideoPause();
-
         }
 
+
+
+        private void btn_Stop_Click(object sender, RoutedEventArgs e)
+        {
+            play.video.Stop();
+            mNowPlayingList.mIsPlay = false;
+        }
+
+        private void btn_Next_Click(object sender, RoutedEventArgs e)
+        {
+            if (++mNowPlayingList.mNowPlayingOrder == mNowPlayingList.mMediaList.Count)
+                mNowPlayingList.mNowPlayingOrder = 0;
+            if (mNowPlayingList.mMediaList[mNowPlayingList.mNowPlayingOrder].mPath != null)
+            {
+                mNowPlayingList.mNowMediaPosition = 0;
+                play.Music_Open(mNowPlayingList.mMediaList[mNowPlayingList.mNowPlayingOrder].mPath);
+                
+            }
+        }
+
+        private void btn_Prev_Click(object sender, RoutedEventArgs e)
+        {
+            if (--mNowPlayingList.mNowPlayingOrder < 0)
+                mNowPlayingList.mNowPlayingOrder = mNowPlayingList.mMediaList.Count - 1;
+            if (mNowPlayingList.mMediaList[mNowPlayingList.mNowPlayingOrder].mPath != null)
+            {
+                mNowPlayingList.mNowMediaPosition = 0;
+                play.Music_Open(mNowPlayingList.mMediaList[mNowPlayingList.mNowPlayingOrder].mPath);
+            }
+        }
     }
 
 }

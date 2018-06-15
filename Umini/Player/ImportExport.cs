d5b7319474b9418107ec78a13b1def1c2538fd1a
pansyscent <pansyscent@gmail.com>
2018-06-15 16:58:19 +0900

    Add Download State

---
+++ Umini/Player/ImportExport.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace Player
+{
+    public class ImportExport
+    {
+        /// <summary>
+        /// import /account/{name}.json to Account.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Account importAccount(string name)
+        {
+            JavaScriptSerializer json = new JavaScriptSerializer();
+            string readJson;
+            try
+            {
+                 readJson = File.ReadAllText(Path.Combine("account", name + ".json" ));
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+            return json.Deserialize<Account>(readJson);
+        }
+
+        /// <summary>
+        /// export Account to json at /account/{name}.json
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        public int exportAccount(Account account)
+        {
+            JavaScriptSerializer json = new JavaScriptSerializer();
+            string writeJson = json.Serialize(account);
+            try
+            {
+                File.WriteAllText(Path.Combine(makeFolder("account"), account.mID + ".json"), writeJson);
+            }
+            catch(Exception ex)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+
+        public string makeFolder(string folderName)
+        {
+            Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), folderName));
+            return Path.Combine(Directory.GetCurrentDirectory(), folderName);
+        }
+    }
+}
---
+++ Umini/Player/Player.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player
+{
+    public class Account
+    {
+        public string mID { get; set; }
+        List<Playlist> mPlaylistList { get; set; }
+
+        public Account()
+        {
+            mID = null;
+            mPlaylistList = new List<Playlist>();
+        }
+    }
+
+    public class NowPlayingList
+    {
+        public bool mIsPlay { get; set; }
+        public int mNowPlayingOrder { get; set; }   // now playing media's Order in playlist 
+        public double mNowMediaPosition { get; set; }    // now playing media's position
+        public List<MediaFile> mMediaList { get; set; }
+
+        public NowPlayingList()
+        {
+            mIsPlay = false;
+            mNowPlayingOrder = 0;
+            mNowMediaPosition = 0;
+            mMediaList = new List<MediaFile>();
+        }
+    }
+
+    public class Playlist
+    {
+        List<MediaFile> mMediaList { get; set; }
+
+        private string mName { get; set; }
+        private string mAccount { get; set; }
+    }
+
+    public class Youtube : MediaFile
+    {
+        public string mURL { get; set; }
+        public string mYoutubeID { get; set; }
+        public string mYoutubeTitle { get; set; }
+
+        public Youtube()
+        {
+            mURL = null;
+            mYoutubeID = null;
+            mYoutubeTitle = null;
+        }
+    }
+
+
+    public class MediaFile
+    { // mp3,wmv,  mp4
+      // file's information
+
+        public string mType { get; set; }  // file type
+        public string mExt { get; set; }
+        public string mPath { get; set; }
+        public double mLength { get; set; }
+
+        // media tag
+        public string mTitle { get; set; }
+        public string mArtist { get; set; }
+        public string mAllbum { get; set; }
+        public string mYear { get; set; }
+        public int mTrack { get; set; }
+        public string mGenre { get; set; }
+        public string mcomment { get; set; }
+        public string mImagePath { get; set; }
+        public string mLyric { get; set; }
+    }
+}
